541f8ecf7cdbe8009e7aa270bc06e06a55301356
Janguk.kim <Janguk.kim@hd.com>
2024-09-24 18:18:27 +0900

    add: [한양대 HCPC 2023] 개표
    [한양대 HCPC 2023] 개표
     - 문제 해결

--- Softeer_LV1/HCPC_2023_vote_counting.c
+++ Softeer_LV1/HCPC_2023_vote_counting.c
@@ -3,5 +3,58 @@
     Created on:     2024-09-23
     file name:      HCPC_2023_vote_counting.c
     problem name:   [한양대 HCPC 2023] 개표
-    status:         Ongoing
+    status:         Clear
 */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int T;  // 후보의 수
+    int n;  // 각 후보가 받은 표의 수
+    int NUM_VOTE[100];
+    int Five_Count[100] = {0, };
+    int Single_Count[100] = {0, };
+
+    /* 입력 - 후보의 수 */
+    scanf("%d", &T);    
+
+    /* 입력 - 각 부호가 받은 표의 수 */
+    for(int i = 0; i < T; i++)
+    {
+        scanf("%d", &n);
+        NUM_VOTE[i] = n;
+    }
+    
+    for(int i = 0; i < T; i++)
+    {
+        Five_Count[i] = NUM_VOTE[i] / 5;
+        Single_Count[i] = NUM_VOTE[i] % 5;
+    }
+
+    for(int i = 0; i < T; i++)
+    {
+        for(int j = 0; j < Five_Count[i]; j++)
+        {
+            printf("++++ ");
+        }
+        for(int j = 0; j < Single_Count[i]; j++)
+        {
+            printf("|");
+        }
+        printf("\n");
+    }
+
+    /* 변수 디버깅 용도 print */
+    // for(int i = 0; i < T; i++)
+    // {
+    //     printf("%d\n", Five_Count[i]);
+    // }
+    // printf("\n");
+    // for(int i = 0; i < T; i++)
+    // {
+    //     printf("%d\n", Single_Count[i]);
+    // }
+
+    return 0;
+}
