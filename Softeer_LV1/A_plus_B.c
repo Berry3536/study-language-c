a55fa5950c9f209bc56a2489646a5fb8b88a6d5f
Janguk.kim <Janguk.kim@hd.com>
2024-09-25 16:56:44 +0900

    add: 문제 해결
    문제 해결
     - A+B
     - 주행거리 비교하기

--- Softeer_LV1/A_plus_B.c
+++ Softeer_LV1/A_plus_B.c
@@ -3,5 +3,33 @@
     Created on:     2024-09-23
     file name:      A_plus_B.c
     problem name:   A+B
-    status:         Ongoing
+    status:         Clear
 */
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    int T;          // 테스트 케이스의 개수
+    int A[100];     // 정수 A
+    int B[100];     // 정수 B
+    int SUM_of_A_B[100];     // 정수 A와 B의 합
+
+    /* 입력 - 테스트 케이스의 개수 */
+    scanf("%d", &T);
+
+    /* 입력 - 정수 A, B */
+    for(int i=0; i<T; i++)
+    {
+        scanf("%d %d", &A[i], &B[i]);
+        SUM_of_A_B[i] = A[i] + B[i];
+    }
+
+    /* 출력 - 정수 A와 B의 합 */
+    for(int i=0; i<T; i++)
+    {
+        printf("Case #%d: %d\n", i+1, SUM_of_A_B[i]);
+    }
+
+}
--- Softeer_LV1/comparision_of_driving_distance.c
+++ Softeer_LV1/comparision_of_driving_distance.c
@@ -3,5 +3,35 @@
     Created on:     2024-09-23
     file name:      comparision_of_driving_distance.c
     problem name:   주행거리 비교하기
-    status:         Ongoing
+    status:         Clear
 */
+
+#include <stdio.h>
+
+
+int main(void)
+{
+    int distance_A;     // A 차량의 주행거리
+    int distance_B;     // B 차량의 주행거리
+
+    /* 입력 - A, B 차랑의 주행거리 */
+    scanf("%d %d", &distance_A, &distance_B);
+
+    /* 출력 - A, B 차량의 주행거리 비교 */
+    if(distance_A > distance_B)
+    {
+        printf("A");
+    }
+    else if(distance_A < distance_B)
+    {
+        printf("B");
+    }
+    else if(distance_A == distance_B)
+    {
+        printf("same");
+    }
+    else
+    {
+        // N/A
+    }
+}
